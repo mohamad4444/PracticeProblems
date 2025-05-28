7dc84b6a345711924cdeb5b056bd3c0a17134e1f
Mohammad Mahamid <61114936+mohamad4444@users.noreply.github.com>
2025-05-28 05:07:52 +0200

    Problem 14 with time analysis

--- LeetCode/Problem14.java
+++ LeetCode/Problem14.java
@@ -4,41 +4,67 @@ import java.util.List;
 
 class Solution {
     public static String longestCommonPrefix(String[] strs) {
-        int common = 0;
+        long startTotal = System.nanoTime();
 
+        long startInit = System.nanoTime();
+        int common = 0;
         boolean flag = true;
-        if(strs.length==0){
-            flag=false;
+        if (strs.length == 0) {
+            flag = false;
         }
-        if(strs.length==1){
+        if (strs.length == 1) {
             return strs[0];
         }
+        StringBuilder result = new StringBuilder();
+        long endInit = System.nanoTime();
+
+        long loopTime = 0;
+        long comparisonTime = 0;
+        long appendTime = 0;
+
         while (flag) {
+            long startLoop = System.nanoTime();
             for (int i = 0; i < strs.length - 1; i++) {
-                if (common < strs[i].length() && common < strs[i+1].length()) {
+                long startComp = System.nanoTime();
+                if (common < strs[i].length() && common < strs[i + 1].length()) {
                     if (strs[i].charAt(common) != strs[i + 1].charAt(common)) {
-                        flag = false;
-                        break;
+                        printTimings(endInit - startInit, loopTime, comparisonTime, appendTime, startTotal);
+                        return result.toString();
                     }
                 } else {
-                    flag = false;
+                    printTimings(endInit - startInit, loopTime, comparisonTime, appendTime, startTotal);
+                    return result.toString();
                 }
+                comparisonTime += System.nanoTime() - startComp;
             }
-            if (flag == true) {
-                common++;
-            } else {
-                break;
-            }
-        }
-        return strs[0].substring(0, common);
+            long startAppend = System.nanoTime();
+            result.append(strs[0].charAt(common));
+            appendTime += System.nanoTime() - startAppend;
 
+            common++;
+            loopTime += System.nanoTime() - startLoop;
+        }
+
+        long endTotal = System.nanoTime();
+
+        printTimings(endInit - startInit, loopTime, comparisonTime, appendTime, startTotal);
+
+        return result.toString();
     }
 
+    private static void printTimings(long initTime, long loopTime, long compTime, long appendTime, long startTotal) {
+        long endTotal = System.nanoTime();
+        System.out.println("Initialization time (ms): " + initTime / 1_000_000.0);
+        System.out.println("Loop total time (ms): " + loopTime / 1_000_000.0);
+        System.out.println("Character comparison time (ms): " + compTime / 1_000_000.0);
+        System.out.println("StringBuilder append time (ms): " + appendTime / 1_000_000.0);
+        System.out.println("Total execution time (ms): " + (endTotal - startTotal) / 1_000_000.0);
+    }
     public static void main(String[] args) {
-        String[] strs = new String[] { "flower", "flow", "flight" };
+        String[] strs1 = new String[] { "flower", "flow", "flight" };
         String[] strs2 = new String[] {"dog","racecar","car" };
         String[] strs3 = new String[] {""};
         System.out.println("\nHello world");
-        System.out.println(longestCommonPrefix(strs3));
+        System.out.println(longestCommonPrefix(strs1));
     }
 }
